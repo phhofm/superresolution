dec90ffcf093fb6c057399b7de517e2116330133
Philip Hofmann <philiphofmann@hotmail.com>
2023-05-23 17:16:25 +0200

    update modelsused page

--- generateUsedModelsList.mjs
+++ generateUsedModelsList.mjs
@@ -7,16 +7,16 @@ import Os from 'os'
 // Create Set so we can add only unique items so we get all the models
 let models = new Set();
 
-// traverse one kind of each example (since i used different models for anime for example than photos)
-let examples = ['multimodel/current/photos/buddy', 'multimodel/current/ai_generated/child', 'multimodel/current/anime/fate', 'deblur/GOPRO_Large/test/GOPR0384_11_00/output', 'denoise/output/directions', 'facerestoration/buddy']
+// Add examples here that use all the models. I use specific examples because the input image names etc would show up as models, specifically "chilling","couple","family", "child__4x_NMKDSuperscale","grossermythen__4x_NMKDSuperscale","landscape__4x_NMKDSuperscale","lightning__4x_NMKDSuperscale","livingroom__4x_NMKDSuperscale","painting__4x_NMKDSuperscale", and so forth, generated example with no examples given
+let examples = ['output/lossless/generated/life', 'output/lossless/artifactsremoval/gate2', 'output/lossless/interpolated/fate', 'deblur/GOPRO_Large/test/GOPR0384_11_00/output', 'denoise/output/directions', 'facerestoration/buddy', 'colorization/couple']
 
-// dont use windows, i mean it works here but for the examples on the markdown pages I pass as a props a hardcoded relative path string.
-// on windows /images/ .. would look like \\images\\, so on the vue image comparison component i would need to check for os and replace all / with \\ in the file path strings.
-// But I mean i just shall not switch between windows and ubuntu machines for development. Because for the github build job I have defined ubuntu-latest for the build.
-if (Os.platform() === 'win32') {
-  console.log('Windows system detected, using backslashes')
-  examples = ['multimodel\\curren\\photos\\buddy', 'multimodel\\current\\ai_generated\\child', 'multimodel\\current\\anime\\fate', 'deblur\\GOPRO_Large\\test\\GOPR0384_11_00\\output', 'denoise\\output\\directions', 'facerestoration\\buddy']
-}
+/*
+  let modelNames = data.map(file => file.basename)
+  modelNames.forEach(name => {
+    models.add(name)
+  })
+*/
+
 
 examples.forEach(example => {
   // get the relevant files and extract model names
@@ -28,7 +28,6 @@ examples.forEach(example => {
     models.add(name);
   })
 });
-
 
 
 await fs.writeFile('./models.json', JSON.stringify([...models]), (err) => {
